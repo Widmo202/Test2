a1eaab6dff708b4d39b7961f0a1b8b3079b2ef44
s28897 <s28897@pjwstk.edu.pl>
2024-06-21 18:00:37 +0200

    bd done

--- Test/Test/Data/DatabaseContext.cs
+++ Test/Test/Data/DatabaseContext.cs
@@ -1,4 +1,6 @@
-﻿namespace Test.Data;
+﻿using Test.Models;
+
+namespace Test.Data;
 using Microsoft.EntityFrameworkCore;
 
 public class DatabaseContext : DbContext
@@ -10,10 +12,67 @@ public class DatabaseContext : DbContext
     public DatabaseContext(DbContextOptions options) : base(options)
     {
     }
-    
+    public DbSet<Backpack> Backpacks { get; set; }
+    public DbSet<Character> Characters { get; set; }
+    public DbSet<CharacterTitle> CharacterTitles { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Title> Titles { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Item>().HasData(new List<Item>
+        {
+            new Item
+            {
+                Id = 1,
+                Name = "item1",
+                Weight = 20
+            }
+        });
+        modelBuilder.Entity<Title>().HasData(new List<Title>
+        {
+            new Title
+            {
+                Id = 1,
+                Name = "Amazing"
+            }
+            
+        });
+        modelBuilder.Entity<Character>().HasData(new List<Character>
+        {
+            new Character
+            {
+                Id = 1,
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                CurrentWeight = 10,
+                MaxWeight = 200
+            }
+            
+        });
+        
+        modelBuilder.Entity<Backpack>().HasData(new List<Backpack>
+        {
+            new Backpack
+            {
+                CharacterId = 1,
+                ItemId = 1,
+                Amount = 4
+            }
+        });
+        modelBuilder.Entity<CharacterTitle>().HasData(new List<CharacterTitle>
+        {
+            new CharacterTitle
+            {
+                CharacterId = 1,
+                TitleId = 1,
+                AcquiredAt = DateTime.Parse("2024-05-29")
+                
+            }
+        });
     }
+    
 }
---
+++ Test/Test/Models/CharacterTitle.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test.Models;
+
+[Table("character_titles")]
+[PrimaryKey(nameof(CharacterId), nameof(TitleId))]
+public class CharacterTitle
+{
+    public int CharacterId { get; set; }
+    public int TitleId { get; set; }
+    public DateTime AcquiredAt { get; set; }
+    
+    [ForeignKey(nameof(CharacterId))]
+    public Character Character { get; set; } = null!;
+    [ForeignKey(nameof(TitleId))]
+    public Title Title { get; set; } = null!;
+}
